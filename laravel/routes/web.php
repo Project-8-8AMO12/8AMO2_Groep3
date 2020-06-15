bfdb11b4d5f951c2268e0212e328351e9f29897e
Matthijs <1042400@mborijnland.nl>
2020-06-15 11:42:47 +0200

    changes to web.php

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -11,7 +11,12 @@
 |
 */
 
+Route::get('/', 'IndexController@index')->name('index');
 Route::get('/login','AccountController@indexLogin')->name('login');
 Route::post('/login','AccountController@doLogin')->name('doLogin');
 Route::get('/register','AccountController@indexRegister')->name('register');
 Route::post('/register','AccountController@doRegister')->name('doRegister');
+
+//
+
+Route::get('/contact', 'ContactController@indexContact')->name('contact');
