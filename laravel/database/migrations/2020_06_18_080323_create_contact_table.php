b4313f055fd91a89c5eda2a82f0ae1adb496cc85
Matthijs <1042400@mborijnland.nl>
2020-06-21 15:47:54 +0200

    nav updated

---
+++ laravel/database/migrations/2020_06_18_080323_create_contact_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContactTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contact', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('question_to');
+            $table->string('user_email');
+            $table->string('content', 1000);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contact');
+    }
+}
