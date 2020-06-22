f91738c2bbb43d4d3cf8cac2a27c79a4923e5e6b
IvanHolwerda <47853549+IvanHolwerda@users.noreply.github.com>
2020-06-22 09:33:38 +0200

    De vereniging #14

--- laravel/public/js/app.js
+++ laravel/public/js/app.js
@@ -49857,8 +49857,8 @@ window.hideMobileNavigation = function () {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Sander\Documents\Projects\8AMO2_Groep3\laravel\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Sander\Documents\Projects\8AMO2_Groep3\laravel\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\owo\8AMO2_Groep3\laravel\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\owo\8AMO2_Groep3\laravel\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- laravel/resources/views/page/vereniging.blade.php
+++ laravel/resources/views/page/vereniging.blade.php
@@ -2,15 +2,15 @@
 
 @section('content')
 
-<div class="page-vereniging col pb-3">
+<div class="page-vereniging col pb-3 mt-3">
     <div class="row">
         <div class="col page-vereniging__maintitle">
             <h1 class="pt-3">{{ $page_content->title ?? 'De vereniging' }}</h1>
         </div>
     </div>
     <div class="row mt-5">
-        <div class="col-md-6 col-xs-6 text-right ">
-            <img class="w-75 rounded" src="{{ asset('images/placeholder.jpg') }}"/>
+        <div class="col-md-6 col-xs-6 text-right d-flex">
+            <img class="w-75 rounded mx-auto mw-100 page-vereniging__image" src="{{ asset('images/placeholder.jpg') }}"/>
         </div>
         <div class="col-md-6 col-xs-6">
             <h2 class="page-vereniging__subtitle">yes this title</h2>
@@ -27,8 +27,8 @@
             <p>{!! $page_content->content_block_2 ?? 'Content block 2' !!}</p>
 
         </div>
-        <div class="col-md-6 col-xs-6 ">
-            <img class="w-75 rounded" src="{{ asset('images/placeholder.jpg') }}"/>
+        <div class="col-md-6 col-xs-6 d-flex">
+            <img class="w-75 rounded mx-auto mw-100 page-vereniging__image" src="{{ asset('images/placeholder.jpg') }}"/>
         </div>
     </div>
     <div class="row mt-3">
@@ -41,15 +41,47 @@
 
     </div>
 
-    <div class="row">
-        <div class="col">
-            <div class="page-vereniging__info rounded-lg p-2 mt-3">De contributie voor leden bedraagt voor 2020 € 77,50 per jaar.
 
-                Leden van de imkersvereniging Leiden en omstreken zijn tevens lid van de NBV. De bijenvolken van de leden zijn verzekerd. Leden ontvangen het maandblad ‘Bijen’ en ‘De Stertselaar’.
-                De donatie voor belangstellenden bedraagt ten minste € 15,- per jaar.
-                Donateurs ontvangen ‘De Stertselaar’.</div>
+    <div class="row mt-5">
+        <div class="col-md-6 col-xs-6 text-right d-flex">
+            <img class="w-75 rounded mx-auto page-vereniging__image" src="{{ asset('images/placeholder.jpg') }}"/>
+        </div>
+        <div class="col-md-6 col-xs-6">
+            <h2 class="page-vereniging__subtitle">yes this title</h2>
+            <h1 class="page-vereniging__title">is made out of title</h1>
+            <p>{!! $page_content->content_block_4 ?? 'Content block 4' !!}</p>
+
         </div>
     </div>
+
+    <div class="row mt-3">
+
+        <div class="col">
+            <h2 class="page-vereniging__subtitle">yes this is a title</h2>
+            <p>{!! $page_content->content_block_5 ?? 'Content block 5' !!}</p>
+
+        </div>
+
+    </div>
+
+    <div class="row mt-5">
+        <div class="col-md-6 col-xs-6 text-right d-flex">
+            <img class="w-75 rounded mx-auto page-vereniging__image" src="{{ asset('images/placeholder.jpg') }}"/>
+        </div>
+        <div class="col-md-6 col-xs-6">
+            <h2 class="page-vereniging__subtitle">yes this title</h2>
+            <h1 class="page-vereniging__title">is made out of title</h1>
+            <p>{!! $page_content->content_block_6 ?? 'Content block 6' !!}</p>
+
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col">
+            <div class="page-vereniging__info rounded-lg p-2 mt-3">{!! $page_content->content_block_7 ?? 'Content block 7' !!}</div>
+        </div>
+    </div>
+
 </div>
 
 @endsection
